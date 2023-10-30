8e98ca3d7827c34581a0ebe5d2272c84418ea45a
abusaleh2234 <you@example.com>
2023-10-31 02:53:11 +0600

    product add / post complete

--- index.js
+++ index.js
@@ -77,32 +77,32 @@ async function run() {
       res.send(result)
 
     })
-    app.put("/updateProduct/:id", async (req, res) => {
-      const id = req.params.id
-      const filter = { _id: new ObjectId(id) }
-      const options = { upsert: true };
-      const product = req.body
-      console.log(product);
-      const updateProduct = {
-        $set: {
-          name: product.name,
-          brandName: product.brandName,
-          image: product.imageUrl,
-          rating: product.rating,
-          price: product.price,
-          type: product.type,
-          details: product.details
-        },
-      }
-      const result = await productsCollection.updateOne(filter, updateProduct, options);
-      res.send(result)
-    })
-    app.delete("/addCart/:id",async (req,res) => {
-      const id = req.params.id
-      const query = {_id: id}
-      const result = await myCardCollection.deleteOne(query)
-      res.send(result)
-    })
+    // app.put("/updateProduct/:id", async (req, res) => {
+    //   const id = req.params.id
+    //   const filter = { _id: new ObjectId(id) }
+    //   const options = { upsert: true };
+    //   const product = req.body
+    //   console.log(product);
+    //   const updateProduct = {
+    //     $set: {
+    //       name: product.name,
+    //       brandName: product.brandName,
+    //       image: product.imageUrl,
+    //       rating: product.rating,
+    //       price: product.price,
+    //       type: product.type,
+    //       details: product.details
+    //     },
+    //   }
+    //   const result = await productsCollection.updateOne(filter, updateProduct, options);
+    //   res.send(result)
+    // })
+    // app.delete("/addCart/:id",async (req,res) => {
+    //   const id = req.params.id
+    //   const query = {_id: id}
+    //   const result = await myCardCollection.deleteOne(query)
+    //   res.send(result)
+    // })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
