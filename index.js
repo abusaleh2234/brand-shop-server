e46a8bb0b94173dca8eb6bd3f4b852ea299cccf5
abusaleh2234 <you@example.com>
2023-10-31 16:19:57 +0600

    add product delete

--- index.js
+++ index.js
@@ -97,12 +97,12 @@ async function run() {
       const result = await productsCollection.updateOne(filter, updateProduct, options);
       res.send(result)
     })
-    // app.delete("/addCart/:id",async (req,res) => {
-    //   const id = req.params.id
-    //   const query = {_id: id}
-    //   const result = await myCardCollection.deleteOne(query)
-    //   res.send(result)
-    // })
+    app.delete("/addCart/:id",async (req,res) => {
+      const id = req.params.id
+      const query = {_id: id}
+      const result = await myCardCollection.deleteOne(query)
+      res.send(result)
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
