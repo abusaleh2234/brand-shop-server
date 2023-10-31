48e56c6a0a217510660868b04bb7499b6d296805
abusaleh2234 <you@example.com>
2023-10-31 20:47:33 +0600

    server all work complete

--- index.js
+++ index.js
@@ -27,7 +27,6 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-    // Send a ping to confirm a successful connection
 
     const brandsCollection = client.db("brandsDB").collection("brands")
     const productsCollection = client.db("brandsDB").collection("productsItem")
@@ -108,7 +107,6 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
